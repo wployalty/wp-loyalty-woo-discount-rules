35acb6e456cfd938a91398764d6ab51b54aa414a
Alagesan <alagesanbe08@gmail.com>
2023-08-09 17:52:43 +0530

    - HPOS support

--- wp-loyalty-woo-discount-rules.php
+++ wp-loyalty-woo-discount-rules.php
@@ -1,16 +1,16 @@
 <?php
 /**
- * Plugin Name: WPLoyalty - Woo Discount Rule Integration
+ * Plugin Name: WPLoyalty - Discount Rule Integration
  * Plugin URI: https://www.wployalty.net
  * Description: Adds Woo Discount Rule conditional support for WPLoyalty
- * Version: 1.0.0
+ * Version: 1.0.1
  * Author: WPLoyalty
  * Slug: wp-loyalty-woo-discount-rule
  * Text Domain: wp-loyalty-woo-discount-rule
  * Domain Path: /i18n/languages/
  * Requires at least: 4.9.0
  * WC requires at least: 6.5
- * WC tested up to: 7.5
+ * WC tested up to: 8.0
  * Contributors: Alagesan
  * Author URI: https://wployalty.net/
  * License: GPLv2 or later
@@ -29,9 +29,6 @@ if (!function_exists('isWoocommerceActive')) {
         return in_array('woocommerce/woocommerce.php', $active_plugins) || array_key_exists('woocommerce/woocommerce.php', $active_plugins);
     }
 }
-if (!isWoocommerceActive()) {
-    return;
-}
 if (!function_exists('isWployaltyActiveOrNot')) {
     function isWployaltyActiveOrNot()
     {
@@ -42,15 +39,21 @@ if (!function_exists('isWployaltyActiveOrNot')) {
         return in_array('wp-loyalty-rules/wp-loyalty-rules.php', $active_plugins, false) || in_array('wp-loyalty-rules-lite/wp-loyalty-rules-lite.php', $active_plugins, false) || in_array('wployalty/wp-loyalty-rules-lite.php', $active_plugins, false);
     }
 }
-if (!isWployaltyActiveOrNot()) {
+if (!isWployaltyActiveOrNot() || !isWoocommerceActive()) {
     return;
 }
 
-defined('WLWD_PLUGIN_NAME') or define('WLWD_PLUGIN_NAME', 'WPLoyalty - Woo Discount Rule Integration');
-defined('WLWD_PLUGIN_VERSION') or define('WLWD_PLUGIN_VERSION', '1.0.0');
+defined('WLWD_PLUGIN_NAME') or define('WLWD_PLUGIN_NAME', 'WPLoyalty - Discount Rule Integration');
+defined('WLWD_PLUGIN_VERSION') or define('WLWD_PLUGIN_VERSION', '1.0.1');
 defined('WLWD_PLUGIN_SLUG') or define('WLWD_PLUGIN_SLUG', 'wp-loyalty-woo-discount-rule');
 defined('WLWD_PLUGIN_PATH') or define('WLWD_PLUGIN_PATH', __DIR__ . '/');
 defined('WLWD_PLUGIN_URL') or define('WLWD_PLUGIN_URL', plugin_dir_url(__FILE__));
+
+add_action('before_woocommerce_init', function () {
+    if (class_exists(\Automattic\WooCommerce\Utilities\FeaturesUtil::class)) {
+        \Automattic\WooCommerce\Utilities\FeaturesUtil::declare_compatibility('custom_order_tables', __FILE__, true);
+    }
+});
 
 if (!class_exists('\Wlwd\App\Router')) {
     if (!file_exists(__DIR__ . '/vendor/autoload.php')) {
